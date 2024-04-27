b99a861ea15f6ce0bec19def1f22c3a7bd3981d7
Udith Kavishan <140702649+MrKavishan@users.noreply.github.com>
2024-04-27 17:59:45 +0530

    updated productservlet

--- ElecZone/src/java/mypackages/productservlet.java
+++ ElecZone/src/java/mypackages/productservlet.java
@@ -1,13 +1,13 @@
-
-package Mypackages;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackages;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.List;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,66 +16,68 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Mr.Kshan
  */
-@WebServlet("/productservlet")
 public class productservlet extends HttpServlet {
 
-   private ProductDov productdov ;
-   
-   public productservlet(){
-       this.productdov=new ProductDov();
-   }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet productservlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet productservlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String action=request.getRequestURI();
-        String [] urlpattern  =action.split("/");
-        String url="";
-        
-        if(urlpattern.length>3){
-                url=urlpattern[3];
-                
-            }
-        else{
-            url="default";
-        }
-        System.out.println(url);
-        
-        switch(url){
-            case "new":{
-                System.out.println("new case");
-                break;
-                }
-            default:{
-                
-            }    
+        processRequest(request, response);
     }
-   
-      
-}
-  
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        this.doGet(request, response);
+        processRequest(request, response);
     }
-        
-        private void showhomepage(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException{
-            try{
-               List<Products> allphones =productdov.selectAllPhones();
-               RequestDispatcher dis =request.getRequestDispatcher("/produt.jsp");
-               request.setAttribute("allphones", allphones);
-               dis.forward(request, response);
-            }
-            catch(IOException | ServletException e){
-                e.printStackTrace();
-            }
-        }
-        
-    
 
-    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
